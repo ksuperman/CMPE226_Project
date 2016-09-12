61f476dccf61d4a32d6b71e2f63ca22614d5d4a1
Pooja Yelure <pooja.yelure@gmail.com>
2016-09-12 10:26:49 -0700

    Added more styling

--- welcome.php
+++ welcome.php
@@ -2,7 +2,7 @@
 <html >
 <head>
 	<meta charset="UTF-8">
-	<title>Sign-Up/Login Form</title>
+	<title>Welcome Page</title>
 	<link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="css/bootstrap.css">
 	<link rel="stylesheet" href="css/signup.css">
@@ -28,93 +28,101 @@
 		</div>
 	</nav>
 
-
-	<div class="fonts"> 
-		<div class="row">
-			<div class="col-sm-4"></div>
-			<div class="col-sm-4"> <h1 class="fonts">User Details</h1> </div>
-			<div class="col-sm-4"></div>
-		</div>
-
-
-
-		<div class="row">
-			<div class="col-sm-4"></div>
-			<div class="col-sm-4">
-
-				<?php
-				$servername = "localhost";
-				$username = "root";
-				$password = "root";
-				$dbname = "ONLINESHOPPING";
+	<div id = "background">
+		<div  class="fonts text-center container-fluid"> 
+			<div class="row">
+				<div class="col-sm-4"></div>
+				<div class="col-sm-4 "> <h1 class="centerBlock fonts">User Details</h1> </div>
+				<div class="col-sm-4"></div>
+			</div>
 
 
-				try {
-					$dbh = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 
-					$sql_stmt = "SELECT FIRST_NAME, LAST_NAME, EMAIL, USER_ID, DATE_OF_BIRTH, GENDER, ADDRESS, CONTACT_NUMBER FROM USER_DETAILS WHERE USER_ID='".$_GET["username"]."';";
-					$sql = $dbh->prepare($sql_stmt);
+			<div class="row">
+				<div class="col-md-4"></div>
+				<div class="col-md-4 ">
 
-					if($sql->execute()) {
-						$sql->setFetchMode(PDO::FETCH_ASSOC);
+					<?php
+					$servername = "localhost";
+					$username = "root";
+					$password = "root";
+					$dbname = "ONLINESHOPPING";
+
+
+					try {
+						$dbh = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+						$sql_stmt = "SELECT FIRST_NAME, LAST_NAME, EMAIL, USER_ID, DATE_OF_BIRTH, GENDER, ADDRESS, CONTACT_NUMBER FROM USER_DETAILS WHERE USER_ID='".$_GET["username"]."';";
+						$sql = $dbh->prepare($sql_stmt);
+
+						if($sql->execute()) {
+							$sql->setFetchMode(PDO::FETCH_ASSOC);
+						}
 					}
-				}
-				catch(Exception $error) {
-					echo '<p>', $error->getMessage(), '</p>';
-				}
+					catch(Exception $error) {
+						echo '<p>', $error->getMessage(), '</p>';
+					}
 
-				?>
-
-
-				<div class= "table-responsive">
-
-					<table class="table-striped">          
-						
-							<?php while($row = $sql->fetch()) { ?>
-
-								<tr>
-									<td>First Name</td>
-									<td><?php echo $row['FIRST_NAME']; ?></td>	        	
-								</tr>
-								<tr>
-									<td>Last Name</td>
-									<td><?php echo $row['LAST_NAME']; ?></td>	        	
-								</tr>
-								<tr>
-									<td>Email</td>
-									<td><?php echo $row['EMAIL']; ?></td>	        	
-								</tr>
-
-								<tr>
-									<td>User id</td>
-									<td><?php echo $row['USER_ID']; ?></td>	        	
-								</tr>
-								<tr>
-									<td>Date of Birth</td>
-									<td><?php echo $row['DATE_OF_BIRTH']; ?></td>	        	
-								</tr>
-								<tr>
-									<td>Gender</td>
-									<td><?php echo $row['GENDER']; ?></td>	        	
-								</tr>
-								<tr>
-									<td>Address</td>
-									<td><?php echo $row['ADDRESS']; ?></td>	        	
-								</tr>
-								<tr>
-									<td>Contact Number</td>
-									<td><?php echo $row['CONTACT_NUMBER']; ?></td>	        	
-								</tr>
-								<?php } ?>
-							</table>
-
-						</div>
+					?>
 					
-				</div>
+
+					<div id = "table-wrapper" class= "table-responsive">
+
+						<table class="table table-striped">
+								<tbody>
+							
+								<?php while($row = $sql->fetch()) { ?>
+
+									<tr>
+										<td>First Name</td>
+										<td><?php echo $row['FIRST_NAME']; ?></td>	        	
+									</tr>
+									<tr>
+										<td>Last Name</td>
+										<td><?php echo $row['LAST_NAME']; ?></td>	        	
+									</tr>
+									<tr>
+										<td>Email</td>
+										<td><?php echo $row['EMAIL']; ?></td>	        	
+									</tr>
+
+									<tr>
+										<td>User id</td>
+										<td><?php echo $row['USER_ID']; ?></td>	        	
+									</tr>
+									<tr>
+										<td>Date of Birth</td>
+										<td><?php echo $row['DATE_OF_BIRTH']; ?></td>	        	
+									</tr>
+									<tr>
+										<td>Gender</td>
+										<td><?php echo $row['GENDER']; ?></td>	        	
+									</tr>
+									<tr>
+										<td>Address</td>
+										<td><?php echo $row['ADDRESS']; ?></td>	        	
+									</tr>
+									<tr>
+										<td>Contact Number</td>
+										<td><?php echo $row['CONTACT_NUMBER']; ?></td>	        	
+									</tr>
+									<?php } ?>
+									</tbody>
+								</table>
+
+							</div>
+						
+					</div>
 
 
-			<div class="col-sm-4"></div>
+				<div class="col-md-4"></div>
+
+				
+				
+			</div>
+			<div class="col-md-4"></div>
 		</div>
 	</div>
+	
 </body>
 </html>
