c3fe75a8e46deaf11903b279b8a598bac90d07e9
Parteek Makkar <parteek_makkar@ymail.com>
2016-10-08 12:12:33 -0700

    Product Sales Query3 Updated

---
+++ orm/ProductSales.php
@@ -0,0 +1,15 @@
+<?php
+        class ProductSales
+        {
+            private $id;
+            private $name;
+            private $price;
+            private $description;
+            
+            public function getId()     { return $this->id; }
+            public function getName()  { return $this->name; }
+            public function getPrice()   { return $this->price; }
+            public function getDescription()   { return $this->description; }
+        }
+?>
+
